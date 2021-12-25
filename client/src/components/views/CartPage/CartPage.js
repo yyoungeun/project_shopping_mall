e0f925708768ad9dfb7af1625d8c1bcac76b5614
yyoungeun <1150amy@naver.com>
2021-12-26 01:09:07 +0900

    Payment 모듈 연결

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { getCartItems, removeCartItem } from "../../../_actions/user_actions";
 import UserCardBlock from "./Sections/UserCardBlock";
 import { Empty } from "antd";
+import Paypal from "../../utils/Paypal";
 
 function CartPage(props) {
   const dispatch = useDispatch();
@@ -68,6 +69,7 @@ function CartPage(props) {
           <Empty description={false} />
         </>
       )}
+      {ShowTotal && <Paypal total={Total} />}
     </div>
   );
 }
